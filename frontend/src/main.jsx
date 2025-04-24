fe70dc98c6d01556dd72c70b8df23a40e2848b2b
LOHAN2000 <anghelovy@outlook.com>
2025-04-24 14:45:55 -0500

    fix: cambiar BrowserRouter a HashRouter para mejorar la compatibilidad en entornos de producción

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
-const base = import.meta.env.MODE === 'production' ? '/proyecto-portafolio-full-stack/' : '/';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={base}>
-      <App />
-    </BrowserRouter>
+      <HashRouter>
+        <App />
+      </HashRouter>
   </StrictMode>
 )

5702d1e5bc37060e20066861860a41de32e527fb
LOHAN2000 <anghelovy@outlook.com>
2025-04-23 22:11:26 -0500

    fix: establecer URL base de la API en la configuración de Axios

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuración global de Axios
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://proyecto-portafolio-full-stack.onrender.com',
   withCredentials: true, // Para enviar cookies (JWT)
 });
 
